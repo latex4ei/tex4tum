0a3bf03a77088cce796eb14ebcd54b9fc1e8c296
Markus Hofbauer <markus.hofbauer@tum.de>
2017-02-11 20:34:00 +0100

    added first version of regexp replacement

--- _plugins/regexp.rb
+++ _plugins/regexp.rb
@@ -15,9 +15,24 @@ module Jekyll
       end
     end
 
+    SINGLE_DOLLAR_REGEXP = %r{(?:\s|^)\$(?=[^\s$])(.*?)(?<=[^\s$\\])\$(?:\s|$)}
+    DUAL_DOLLARD_REGEXP_LINESAVE = %r{(?:^|\n)\s*\$\$(?=[^\s$])(.*?)(?<=[^\s$\\])\$\$\s*(?:\n|$)}
+
     def replace_latex(content)
       latex_definitions = File.read('./res/parser_util/tex_definitions.md')
-      # content += latex_definitions
+      content += "$$#{latex_definitions}$$"
+
+      content = replace_latex_with_regexp(content, SINGLE_DOLLAR_REGEXP)
+      content = replace_latex_with_regexp(content, DUAL_DOLLARD_REGEXP_LINESAVE)
+
+      return content
+    end
+
+    def replace_latex_with_regexp(content, regexp)
+      content = content.gsub(regexp) do |full_match|
+        # The expression returned from this block will be used as the replacement string
+        %Q|<span class="LaTeX">#{full_match.strip}</span>|
+      end
 
       return content
     end
@@ -29,6 +44,6 @@ module Jekyll
       return content
     end
 
-    private :replace_latex, :add_abbreviations
+    private :replace_latex, :replace_latex_with_regexp, :add_abbreviations
   end
 end
