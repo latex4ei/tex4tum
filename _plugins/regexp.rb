0ffed2697327e40a1c741cdc294373f841acd6bd
Markus Hofbauer <markus.hofbauer@tum.de>
2017-02-24 16:30:33 +0100

    resolve #15

--- _plugins/regexp.rb
+++ _plugins/regexp.rb
@@ -4,38 +4,22 @@ module Jekyll
     priority :highest
 
     def generate(site)
-      puts "Replacing Abbreviations"
-      puts "Replacing Latex"
+      puts "Adding Abbreviations"
+      puts "Adding Latex Definitions"
 
       site.collections.each do |label, collection|
         collection.docs.each do |document|
           document.content = add_abbreviations(document.content)
-          document.content = replace_latex(document.content)
+          document.content = add_latex_definitions(document.content)
         end
       end
     end
 
-    SINGLE_DOLLAR_REGEXP = %r{(?:\s|^)\$(?=[^\s$])(.*?)(?<=[^\s$\\])\$(?:\s|$)}
-    DUAL_DOLLARD_REGEXP_LINESAVE = %r{(?:^|\n)\s*\$\$(?=[^\s$])(.*?)(?<=[^\s$\\])\$\$\s*(?:\n|$)}
-
     ABBREVIATION_REGEXP = %r{(\*\[([^\]]+)\]:\s*([^\n]+\n))}
 
-    def replace_latex(content)
+    def add_latex_definitions(content)
       latex_definitions = File.read('./res/parser_util/tex_definitions.md')
-      # content += "$$#{latex_definitions}$$"
       content = latex_definitions + content
-
-      #content = replace_latex_with_regexp(content, SINGLE_DOLLAR_REGEXP)
-      #content = replace_latex_with_regexp(content, DUAL_DOLLARD_REGEXP_LINESAVE)
-
-      return content
-    end
-
-    def replace_latex_with_regexp(content, regexp)
-      content = content.gsub(regexp) do |full_match|
-        # The expression returned from this block will be used as the replacement string
-        %Q|<span class="LaTeX">#{full_match.strip}</span>|
-      end
 
       return content
     end
@@ -49,9 +33,8 @@ module Jekyll
       return content
     end
 
-
-    def parse_abbreviation(content) 
-      content.scan(ABBREVIATION_REGEXP) { |match|
+    def parse_abbreviation(content)
+      content.scan(ABBREVIATION_REGEXP) do |match|
           line = match[0]   #$1
           #puts line
           key = match[1]
@@ -59,13 +42,11 @@ module Jekyll
 
           content = content.gsub(line, '')
           content = content.gsub(/(?<=\W|^)#{key}(?=\W|$)/, '<abbr title="' + value + '" >' + key + '</abbr>')
-      }
+      end
 
       return content
     end
 
-
-
-    private :replace_latex, :replace_latex_with_regexp, :add_abbreviations
+    private :add_latex_definitions, :add_abbreviations, :parse_abbreviation
   end
 end
